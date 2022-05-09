4b8222971fa191397b4c6cf6a176956a96ff7f6e
Eugene <eugenykravchenko@gmail.com>
2022-05-09 13:31:43 +0200

    color change

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const hamburger = document.querySelector('.promo__hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
+const overlay = document.querySelector('.menu__overlay');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
 });
 
 closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+})
+
+overlay.addEventListener('click', () => {
     menu.classList.remove('active');
 })
