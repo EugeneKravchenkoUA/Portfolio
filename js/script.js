0e28b18c78008bdeaa6f0d233340c6470df49b89
Yevhenii Kravchenko <yevhenii@MacBook-Yevhenii.local>
2022-05-23 23:08:22 +0200

    Finished section 'skills'

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const hamburger = document.querySelector('.promo__hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
 const overlay = document.querySelector('.menu__overlay');
+const counters = document.querySelectorAll('.skills__ratings-counter');
+const lines = document.querySelectorAll('.skills__ratings-line span');
 
 hamburger.addEventListener('click', () => {
     menu.classList.add('active');
@@ -9,8 +11,12 @@ hamburger.addEventListener('click', () => {
 
 closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
 
 overlay.addEventListener('click', () => {
     menu.classList.remove('active');
-})
+});
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+}); 
